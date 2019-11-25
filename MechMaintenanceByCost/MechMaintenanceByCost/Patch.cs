c31922aad45ab9a92987f9c1236f92226994e670
David Walker <sq_flasher@hotmail.com>
2019-11-25 17:04:48 -0500

    Patch for BT1.8/

--- MechMaintenanceByCost/MechMaintenanceByCost/Patch.cs
+++ MechMaintenanceByCost/MechMaintenanceByCost/Patch.cs
@@ -1,22 +1,26 @@
 ﻿using BattleTech;
 using BattleTech.UI;
+using BattleTech.UI.TMProWrapper;
 using Harmony;
 using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+
+
 namespace MechMaintenanceByCost {
 
    [HarmonyPatch(typeof(SGCaptainsQuartersStatusScreen), "RefreshData")]
     public static class SGCaptainsQuartersStatusScreen_RefreshData {
-        static void Postfix(SGCaptainsQuartersStatusScreen __instance) {
+        static void Postfix(SGCaptainsQuartersStatusScreen __instance)
+        {
             try {
-                ReflectionHelper.InvokePrivateMethode(__instance, "ClearListLineItems",new object[] { ReflectionHelper.GetPrivateField(__instance, "SectionOneExpensesList") });
+                Traverse.Create(__instance).Method("ClearListLineItems", new Type[] { typeof(Transform) }).GetValue(new object[] { Traverse.Create(__instance).Field("SectionOneExpensesList").GetValue() });
 
                 Settings settings = Helper.LoadSettings();
 
-                SimGameState simState = (SimGameState)ReflectionHelper.GetPrivateField(__instance, "simState");
+                SimGameState simState = Traverse.Create(__instance).Field("simState").GetValue<SimGameState>();
                 float expenditureCostModifier = simState.GetExpenditureCostModifier(simState.ExpenditureLevel);
                 List<KeyValuePair<string, int>> list = new List<KeyValuePair<string, int>>();
                 int ongoingUpgradeCosts = 0;
@@ -48,10 +52,10 @@ namespace MechMaintenanceByCost {
                 list.ForEach(delegate (KeyValuePair<string, int> entry)
                 {
                     ongoingUpgradeCosts += entry.Value;
-                    ReflectionHelper.InvokePrivateMethode(__instance, "AddListLineItem", new object[] { ReflectionHelper.GetPrivateField(__instance, "SectionOneExpensesList"), entry.Key, SimGameState.GetCBillString(entry.Value) });
+                    Traverse.Create(__instance).Method("AddListLineItem", new Type[] { typeof(Transform), typeof(string), typeof(string) }).GetValue(new object[] { Traverse.Create(__instance).Field("SectionOneExpensesList").GetValue(), entry.Key, SimGameState.GetCBillString(entry.Value) });
                 });
 
-                ReflectionHelper.InvokePrivateMethode(__instance, "SetField", new object[] { ReflectionHelper.GetPrivateField(__instance, "SectionOneExpensesField"), SimGameState.GetCBillString(ongoingUpgradeCosts) }, new Type[] { typeof(TextMeshProUGUI), typeof(string)  });
+                Traverse.Create(__instance).Method("SetField", new Type[] { typeof(LocalizableText), typeof(string) }).GetValue(new object[] { Traverse.Create(__instance).Field("SectionOneExpensesField").GetValue<LocalizableText>(), SimGameState.GetCBillString(ongoingUpgradeCosts) } );
             }
             catch (Exception e) {
                 Logger.LogError(e);
@@ -61,25 +65,50 @@ namespace MechMaintenanceByCost {
 
     [HarmonyPatch(typeof(SimGameState), "GetExpenditures")]
     [HarmonyPatch(new Type[] { typeof(EconomyScale), typeof(bool) })]
-    public static class SimGameState_GetExpenditures {
+    public static class SimGameState_GetExpenditures
+    {
 
-        static void Postfix(ref SimGameState __instance, ref int __result) {
-            try {
+        static void Postfix(ref SimGameState __instance, ref int __result, ref bool proRate)
+        {
+            try
+            {
                 Settings settings = Helper.LoadSettings();
                 float expenditureCostModifier = __instance.GetExpenditureCostModifier(__instance.ExpenditureLevel);
-                foreach (MechDef mechDef in __instance.ActiveMechs.Values) {
-                    __result -= Mathf.CeilToInt(__instance.Constants.Finances.MechCostPerQuarter * expenditureCostModifier);
-                    if (settings.CostByTons) {
-                        __result += Mathf.RoundToInt((float)mechDef.Chassis.Tonnage * settings.cbillsPerTon * expenditureCostModifier);
-                        if (settings.TonsAdditive) {
-                            __result += Mathf.RoundToInt(Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost * expenditureCostModifier);
-                        }
-                    } else {
-                        __result += Mathf.RoundToInt(Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost * expenditureCostModifier);
-                    }               
+                __result = 0;
+
+                FinancesConstantsDef finances = __instance.Constants.Finances;
+                __result = __instance.GetShipBaseMaintenanceCost();
+                for (int i = 0; i < __instance.ShipUpgrades.Count; i++)
+                {
+                    __result += Mathf.RoundToInt(expenditureCostModifier * __instance.ShipUpgrades[i].AdditionalCost);
                 }
-            }              
-            catch (Exception e) {
+
+                for (int i = 0; i < __instance.PilotRoster.Count; i++)
+                {
+                    __result += Mathf.RoundToInt(expenditureCostModifier * __instance.GetMechWarriorValue(__instance.PilotRoster[i].pilotDef));
+                }
+
+                foreach (MechDef mechDef in __instance.ActiveMechs.Values)
+                {
+                    if (settings.CostByTons)
+                    {
+                        __result += Mathf.RoundToInt(expenditureCostModifier * (float)mechDef.Chassis.Tonnage * settings.cbillsPerTon);
+                        if (settings.TonsAdditive)
+                        {
+                            __result += Mathf.RoundToInt(expenditureCostModifier * Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost);
+                        }
+                    }
+                    else
+                    {
+                        __result += Mathf.RoundToInt(expenditureCostModifier * Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost);
+                    }
+                }
+
+
+                __result -= Mathf.RoundToInt(expenditureCostModifier * ((!proRate) ? 0 : Traverse.Create(__instance).Field("ProRateRefund").GetValue<int>()));
+            }
+            catch (Exception e)
+            {
                 Logger.LogError(e);
             }
         }
