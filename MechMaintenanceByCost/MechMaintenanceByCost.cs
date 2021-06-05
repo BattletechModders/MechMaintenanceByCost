6c6fdd2281f5b2a9bf8369821256126b8f356517
David Walker <sq_flasher@hotmail.com>
2021-06-05 13:06:41 -0400

    Compatibility Update
    -Changed Prefixes to Postfixes so that this will play nicely with other mods that modify monthly expenses (unless they modify monthly mech cost but that would make this mod redundant anyway)
    
    -The SGCaptainsQuartersStatusScreen RefreshData postfix works by using a reimplemented version of ClearListLineItems()
    -As Line Items are removed they are checked to see if they are a mech
    -If it's not a mech, the key and value are added to the new SectionOneExpenses keyValuePairList thus preserving any values set by a preceding mod's Prefixes or Postfixes
    
    -The SimGameState GetExpenditures PostFix is much simpler
    -For each mech, it subtracts finances.MechCostPerQuarter from the incoming return value before adding the modified cost of the mech

--- MechMaintenanceByCost/MechMaintenanceByCost.cs
+++ MechMaintenanceByCost/MechMaintenanceByCost.cs
@@ -39,99 +39,76 @@ namespace MechMaintenanceByCost
         [HarmonyPatch(typeof(SGCaptainsQuartersStatusScreen), "RefreshData")]
         public static class SGCaptainsQuartersStatusScreen_RefreshData
         {
-            private static readonly MethodInfo methodAddLineItem = AccessTools.Method(typeof(SGCaptainsQuartersStatusScreen), "AddListLineItem");
-            public static bool Prefix(SGCaptainsQuartersStatusScreen __instance, EconomyScale expenditureLevel, bool showMoraleChange, SimGameState ___simState,
-              SGDifficultyIndicatorWidget ___ExpenditureLevelIndicatorWidget, LocalizableText ___ExpenditureLevelField, LocalizableText ___SectionOneExpenseLevel,
-              LocalizableText ___SectionTwoExpenseLevel, SGFinancialForecastWidget ___FinanceWidget, LocalizableText ___MoraleValueField, SGMoraleBar ___MoralBar,
-              Transform ___SectionOneExpensesList, LocalizableText ___SectionOneExpensesField, LocalizableText ___SectionTwoExpensesField,
-              Transform ___SectionTwoExpensesList, LocalizableText ___EndOfQuarterFunds, LocalizableText ___QuarterOperatingExpenses,
-              LocalizableText ___CurrentFunds, List<LocalizableText> ___ExpenditureLvlBtnMoraleFields, List<LocalizableText> ___ExpenditureLvlBtnCostFields)
+            private static void FilterListItems(Transform container, SimGameState simState, List<string> mechNames, List<KeyValuePair<string, int>> keyValuePairList)
             {
-                if (__instance == null || ___simState == null)
+                List<GameObject> list = new List<GameObject>();
+                System.Collections.IEnumerator enumerator = container.GetEnumerator();
+                while (enumerator.MoveNext())
                 {
-                    return true;
-                }
-                float expenditureCostModifier = ___simState.GetExpenditureCostModifier(expenditureLevel);
-                Traverse methodSetField = Traverse.Create(__instance)
-                  .Method("SetField", new Type[] { typeof(LocalizableText), typeof(string) });
-                int expLevel = (int)Traverse.Create(__instance)
-                  .Method("GetExpendetureLevelIndexNormalized", new object[] { expenditureLevel }).GetValue();
-                ___ExpenditureLevelIndicatorWidget.SetDifficulty(expLevel * 2);
-                methodSetField.GetValue(new object[] { ___ExpenditureLevelField, string.Format("{0}", (object)expenditureLevel) });
-                methodSetField.GetValue(new object[] { ___SectionOneExpenseLevel, string.Format("{0}", (object)expenditureLevel) });
-                methodSetField.GetValue(new object[] { ___SectionTwoExpenseLevel, string.Format("{0}", (object)expenditureLevel) });
-                ___FinanceWidget.RefreshData(expenditureLevel);
-                int num1 = ___simState.ExpenditureMoraleValue[expenditureLevel];
-                methodSetField.GetValue(new object[] { ___MoraleValueField, string.Format("{0}{1}", num1 > 0 ? (object)"+" : (object)"", (object)num1) });
-                if (showMoraleChange)
-                {
-                    int morale = ___simState.Morale;
-                    ___MoralBar.ShowMoraleChange(morale, morale + num1);
-                }
-                else
-                    ___MoralBar.ShowCurrentMorale();
-                Traverse.Create(__instance).Method("ClearListLineItems", new object[] { ___SectionOneExpensesList }).GetValue();
-                List<KeyValuePair<string, int>> keyValuePairList = new List<KeyValuePair<string, int>>();
-                int ongoingUpgradeCosts = 0;
-                string key = ___simState.CurDropship == DropshipType.Leopard ? Strings.T("Bank Loan Interest Payment") : Strings.T("Argo Operating Costs");
-                int num2 = Mathf.RoundToInt(expenditureCostModifier * (float)___simState.GetShipBaseMaintenanceCost());
-                keyValuePairList.Add(new KeyValuePair<string, int>(key, num2));
-                foreach (ShipModuleUpgrade shipUpgrade in ___simState.ShipUpgrades)
-                {
-                    if (___simState.CurDropship == DropshipType.Argo && Mathf.CeilToInt((float)shipUpgrade.AdditionalCost * ___simState.Constants.CareerMode.ArgoMaintenanceMultiplier) > 0)
-                    {
-                        string name = shipUpgrade.Description.Name;
-                        int num3 = Mathf.RoundToInt(expenditureCostModifier * (float)Mathf.CeilToInt((float)shipUpgrade.AdditionalCost * ___simState.Constants.CareerMode.ArgoMaintenanceMultiplier));
-                        keyValuePairList.Add(new KeyValuePair<string, int>(name, num3));
-                    }
-                }
-                foreach (MechDef mechDef in ___simState.ActiveMechs.Values)
-                {
-                    string name = mechDef.Name;
-                    int num3 = Mathf.RoundToInt(expenditureCostModifier * Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost);
-                    if (settings.CostByTons)
-                    {
-                        num3 = Mathf.RoundToInt(expenditureCostModifier * (float)mechDef.Chassis.Tonnage * settings.cbillsPerTon);
-                        if (settings.TonsAdditive)
-                            num3 += Mathf.RoundToInt(expenditureCostModifier * Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost);
-                    }                    
-                    keyValuePairList.Add(new KeyValuePair<string, int>(name, num3));
-                }
-                keyValuePairList.Sort((Comparison<KeyValuePair<string, int>>)((a, b) => b.Value.CompareTo(a.Value)));
-                keyValuePairList.ForEach((Action<KeyValuePair<string, int>>)(entry =>
-                {
-                    ongoingUpgradeCosts += entry.Value;
-                    methodAddLineItem.Invoke(__instance, new object[] { ___SectionOneExpensesList, entry.Key, SimGameState.GetCBillString(entry.Value) });
-                }));
-                methodSetField.GetValue(new object[] { ___SectionOneExpensesField, SimGameState.GetCBillString(ongoingUpgradeCosts) });
-                keyValuePairList.Clear();
-                Traverse.Create(__instance).Method("ClearListLineItems", new object[] { ___SectionTwoExpensesList }).GetValue();
-                int ongoingMechWariorCosts = 0;
-                foreach (Pilot pilot in ___simState.PilotRoster)
-                {
-                    string displayName = pilot.pilotDef.Description.DisplayName;
-                    int num3 = Mathf.CeilToInt(expenditureCostModifier * (float)___simState.GetMechWarriorValue(pilot.pilotDef));
-                    keyValuePairList.Add(new KeyValuePair<string, int>(displayName, num3));
-                }
-                keyValuePairList.Sort((Comparison<KeyValuePair<string, int>>)((a, b) => b.Value.CompareTo(a.Value)));
-                keyValuePairList.ForEach((Action<KeyValuePair<string, int>>)(entry =>
-                {
-                    ongoingMechWariorCosts += entry.Value;
-                    methodAddLineItem.Invoke(__instance, new object[] { ___SectionTwoExpensesList, entry.Key, SimGameState.GetCBillString(entry.Value) });
-                }));
-                methodSetField.GetValue(new object[] { ___SectionTwoExpensesField, SimGameState.GetCBillString(ongoingMechWariorCosts) });
-                methodSetField.GetValue(new object[] { ___EndOfQuarterFunds, SimGameState.GetCBillString(___simState.Funds + ___simState.GetExpenditures(false)) });
-                methodSetField.GetValue(new object[] { ___QuarterOperatingExpenses, SimGameState.GetCBillString(___simState.GetExpenditures(false)) });
-                methodSetField.GetValue(new object[] { ___CurrentFunds, SimGameState.GetCBillString(___simState.Funds) });
-                int index = 0;
-                foreach (KeyValuePair<EconomyScale, int> keyValuePair in ___simState.ExpenditureMoraleValue)
-                {
-                    ___ExpenditureLvlBtnMoraleFields[index].SetText(string.Format("{0}", (object)keyValuePair.Value), (object[])Array.Empty<object>());
-                    ___ExpenditureLvlBtnCostFields[index].SetText(SimGameState.GetCBillString(___simState.GetExpenditures(keyValuePair.Key, false)), (object[])Array.Empty<object>());
-                    ++index;
+                    object obj = enumerator.Current;
+                    Transform transform = (Transform)obj;
+                    GameObject gameObject = transform.gameObject;
+                    list.Add(gameObject);
                 }
 
-                return false;
+                foreach (GameObject gameObject in list)
+                {
+                    SGKeyValueView component = gameObject.GetComponent<SGKeyValueView>();
+                    string key = Traverse.Create(component).Field("Key").GetValue<LocalizableText>().OriginalText;
+                    string valueString = Traverse.Create(component).Field("Value").GetValue<LocalizableText>().OriginalText;
+                    valueString = valueString.Replace("¢", "").Replace(",", "");
+                    int value = int.Parse(valueString);
+                    if (!mechNames.Contains(key))
+                        keyValuePairList.Add(new KeyValuePair<string, int>(key, value));
+                    
+                    simState.DataManager.PoolGameObject("uixPrfPanl_captainsQuarters_quarterlyReportLineItem-element", gameObject);
+                }
+
+            }
+            static void Postfix(SGCaptainsQuartersStatusScreen __instance, EconomyScale expenditureLevel, SimGameState ___simState,
+                Transform ___SectionOneExpensesList, LocalizableText ___SectionOneExpensesField)
+            {
+                try
+                {
+                    List<KeyValuePair<string, int>> keyValuePairList = new List<KeyValuePair<string, int>>();
+                    float expenditureCostModifier = ___simState.GetExpenditureCostModifier(expenditureLevel);
+                    string sectionOneExpenses = ___SectionOneExpensesField.OriginalText;
+                    sectionOneExpenses = sectionOneExpenses.Replace("¢", "").Replace(",", "");
+                    int ongoingUpgradeCosts = int.Parse(sectionOneExpenses);
+
+                    List<string> mechNames = new List<string>();
+                    foreach (MechDef mechDef in ___simState.ActiveMechs.Values)
+                    {
+                        string key = mechDef.Name;
+                        mechNames.Add(key);
+                        int value = Mathf.RoundToInt(expenditureCostModifier * (float)___simState.Constants.Finances.MechCostPerQuarter);
+                        ongoingUpgradeCosts -= value;
+                        if (settings.CostByTons)
+                        {
+                            value = Mathf.RoundToInt(expenditureCostModifier * (float)mechDef.Chassis.Tonnage * settings.cbillsPerTon);
+                            if (settings.TonsAdditive)
+                                value += Mathf.RoundToInt(expenditureCostModifier * Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost);
+                        }
+                        else
+                            value = Mathf.RoundToInt(expenditureCostModifier * Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost);
+
+                        ongoingUpgradeCosts += value;
+                        keyValuePairList.Add(new KeyValuePair<string, int>(key, value));
+                    }
+                    FilterListItems(___SectionOneExpensesList, ___simState, mechNames, keyValuePairList);
+                    keyValuePairList.Sort((Comparison<KeyValuePair<string, int>>)((a, b) => b.Value.CompareTo(a.Value)));
+                    keyValuePairList.ForEach((Action<KeyValuePair<string, int>>)(entry =>
+                    {
+                        Traverse.Create(__instance).Method("AddListLineItem", new Type[] { typeof(Transform), typeof(string), typeof(string) }).GetValue(
+                            new object[] { ___SectionOneExpensesList, entry.Key, SimGameState.GetCBillString(entry.Value) });
+                    }));                  
+                    Traverse.Create(__instance).Method("SetField", new Type[] { typeof(LocalizableText), typeof(string) }).GetValue(
+                        new object[] { ___SectionOneExpensesField, SimGameState.GetCBillString(ongoingUpgradeCosts) });
+                }
+                catch (Exception e)
+                {
+                    Helper.Logger.LogError(e);
+                }
             }
         }
 
@@ -139,14 +116,16 @@ namespace MechMaintenanceByCost
         [HarmonyPatch(new Type[] { typeof(EconomyScale), typeof(bool) })]
         public static class SimGameState_GetExpenditures
         {
-            public static bool Prefix(SimGameState __instance, EconomyScale expenditureLevel, bool proRate, int ___ProRateRefund, ref int __result)
+            public static void Postfix(SimGameState __instance, EconomyScale expenditureLevel, ref int __result)
             {
-                int baseMaintenanceCost = __instance.GetShipBaseMaintenanceCost();
+
+                FinancesConstantsDef finances = __instance.Constants.Finances;
                 float expenditureCostModifier = __instance.GetExpenditureCostModifier(expenditureLevel);
-                for (int index = 0; index < __instance.ShipUpgrades.Count; ++index)
-                    baseMaintenanceCost += Mathf.CeilToInt((float)__instance.ShipUpgrades[index].AdditionalCost * __instance.Constants.CareerMode.ArgoMaintenanceMultiplier);
+                int baseMaintenanceCost = __result;
+
                 foreach (MechDef mechDef in __instance.ActiveMechs.Values)
                 {
+                    baseMaintenanceCost -= finances.MechCostPerQuarter;
                     if (settings.CostByTons)
                     {
                         baseMaintenanceCost += Mathf.RoundToInt((float)mechDef.Chassis.Tonnage * settings.cbillsPerTon * expenditureCostModifier);
@@ -156,10 +135,7 @@ namespace MechMaintenanceByCost
                     else
                         baseMaintenanceCost += Mathf.RoundToInt(Helper.CalculateCBillValue(mechDef) * settings.PercentageOfMechCost * expenditureCostModifier);
                 }
-                for (int index = 0; index < __instance.PilotRoster.Count; ++index)
-                    baseMaintenanceCost += __instance.GetMechWarriorValue(__instance.PilotRoster[index].pilotDef);
-                __result = Mathf.CeilToInt((float)(baseMaintenanceCost - (proRate ? ___ProRateRefund : 0)) * expenditureCostModifier);
-                return false;
+                __result = baseMaintenanceCost;
             }
         }
 
@@ -174,7 +150,7 @@ namespace MechMaintenanceByCost
                     {
                         writer.WriteLine("Message :" + ex.Message + "<br/>" + Environment.NewLine + "StackTrace :" + ex.StackTrace +
                            "" + Environment.NewLine + "Date :" + DateTime.Now.ToString());
-                        writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                        writer.WriteLine(Environment.NewLine + "-------------------------------------------------------------" + Environment.NewLine);
                     }
                 }
 
@@ -183,7 +159,7 @@ namespace MechMaintenanceByCost
                     using (StreamWriter writer = new StreamWriter(filePath, true))
                     {
                         writer.WriteLine(line + Environment.NewLine + "Date :" + DateTime.Now.ToString());
-                        writer.WriteLine(Environment.NewLine + "-----------------------------------------------------------------------------" + Environment.NewLine);
+                        writer.WriteLine(Environment.NewLine + "--------------------------------------------------------------" + Environment.NewLine);
                     }
                 }
             }
